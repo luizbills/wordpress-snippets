a713df711cf13b5717319556292c1979e2ac4c9c
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-02-02 10:02:15 -0200

    missing semicolon

--- create-template-shortcode.php
+++ create-template-shortcode.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Function to create shortcodes that renders a template part (using Twig with Timber).
  * Note: install and active the plugin Timber (https://wordpress.org/plugins/timber-library).
@@ -10,25 +9,24 @@ function create_template_shortcode( $name, $template_path, $defaults = '' ) {
 	add_shortcode( $name, function ( $args, $content, $shortcode_name ) use ( $name, $template_path, $defaults ){
 		$data = Timber::get_context();
 		
-    if ( is_string( $defaults ) ) {
+	if ( is_string( $defaults ) ) {
 			$args = wp_parse_args( $args, wp_parse_args( $defaults ) );
 		} elseif ( is_array( $defaults ) ) {
 			$args = wp_parse_args( $args, $defaults );
 		}
-    
-    $data = array_merge($data, $args)
-    
+	
+		$data = array_merge($data, $args);
+	
 		$data['content'] = $content;
 		$data['_shortcode_name'] = $shortcode_name;
 		
 		$data = apply_filters( 'template_shortcode_' . $name, $data );
-    
+	
 		return Timber::compile( $template_path, $data, 600 );
 	} );
 }
 
 /* # Usage
-
 // create the shortcode
 create_template_shortcode( 'button', 'partials/button.twig', array(
 	'link' => '',
@@ -36,12 +34,8 @@ create_template_shortcode( 'button', 'partials/button.twig', array(
 	'class' => '',
   'id' => ''
 ) );
-
 // use in your content
 [button link="https://google.com"]Google[/button]
-
 // or in your code
 do_shortcode( '[button]Click me[/button]' );
-
 */
-
