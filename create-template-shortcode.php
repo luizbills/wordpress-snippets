84eb1ae5f3ee05035aa07e62336bdd25ea72ae65
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-02-03 20:08:08 -0200

    1.1.0

--- create-template-shortcode.php
+++ create-template-shortcode.php
@@ -3,25 +3,17 @@
  * Function to create shortcodes that renders a template part (using Twig with Timber).
  * Note: install and active the plugin Timber (https://wordpress.org/plugins/timber-library).
  *
- * @version 1.0.1
+ * @version 1.1.0
 */
-function create_template_shortcode( $name, $template_path, $defaults = '' ) {
-	add_shortcode( $name, function ( $args, $content, $shortcode_name ) use ( $name, $template_path, $defaults ){
-		$data = Timber::get_context();
-		
-	if ( is_string( $defaults ) ) {
-			$args = wp_parse_args( $args, wp_parse_args( $defaults ) );
-		} elseif ( is_array( $defaults ) ) {
-			$args = wp_parse_args( $args, $defaults );
-		}
-	
-		$data = array_merge($data, $args);
-	
+function create_template_shortcode( $name, $template_path, $defaults = array() ) {
+	add_shortcode( $name, function ( $args, $content, $shortcode_name ) use ( $name, $template_path, $defaults ) {
+		$args = shortcode_atts( $defaults, $args, $shortcode_name ); 
+		$data = array_merge( Timber::get_context() , $args);
+
 		$data['content'] = $content;
 		$data['_shortcode_name'] = $shortcode_name;
-		
-		$data = apply_filters( 'template_shortcode_' . $name, $data );
-	
+		$data = apply_filters( 'template_shortcode_' . $shortcode_name, $data );
+
 		return Timber::compile( $template_path, $data, 600 );
 	} );
 }
