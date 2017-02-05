de9b86b95ef37f07281f4341f97bf6b6501bf209
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-02-04 23:31:50 -0200

    2.0.0

--- create-template-shortcode.php
+++ create-template-shortcode.php
@@ -3,31 +3,38 @@
  * Function to create shortcodes that renders a template part (using Twig with Timber).
  * Note: install and active the plugin Timber (https://wordpress.org/plugins/timber-library).
  *
- * @version 1.1.0
+ * @version 2.0.0
 */
 function create_template_shortcode( $name, $template_path, $defaults = array() ) {
-	add_shortcode( $name, function ( $args, $content, $shortcode_name ) use ( $name, $template_path, $defaults ) {
+	add_shortcode( $name, function ( $args, $content, $shortcode_name ) use ( $template_path, $defaults ) {
 		$args = shortcode_atts( $defaults, $args, $shortcode_name ); 
-		$data = array_merge( Timber::get_context() , $args);
+		$data = array_merge( Timber::get_context(), $args);
 
-		$data['content'] = $content;
-		$data['_shortcode_name'] = $shortcode_name;
-		$data = apply_filters( 'template_shortcode_' . $shortcode_name, $data );
-
-		return Timber::compile( $template_path, $data, 600 );
+		$context['content'] = $content;
+		
+		$context = apply_filters( 'template_shortcode_timber_context', $context, $shortcode_name );
+		$cache = apply_filters( 'template_shortcode_timber_cache_options', array(
+			'expires' => 600,
+			'mode' => TimberLoader::CACHE_USE_DEFAULT
+		), $shortcode_name );
+		
+		$context['_shortcode_name'] = $shortcode_name;
+		
+		return Timber::compile( $template_path, $context, 600, $cache['expires'], $cache['mode'],  );
 	} );
 }
 
-/* # Usage
+/*
+# Usage
+
 // create the shortcode
 create_template_shortcode( 'button', 'partials/button.twig', array(
 	'link' => '',
-	'type' => 'primary',
-	'class' => '',
-  'id' => ''
 ) );
+
 // use in your content
 [button link="https://google.com"]Google[/button]
+
 // or in your code
 do_shortcode( '[button]Click me[/button]' );
 */
