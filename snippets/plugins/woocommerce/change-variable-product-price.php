bd78aca9c8070c40ad6f5325be1ec327b51409f6
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-11-08 10:10:25 -0200

    Update change-variable-product-price.php

--- snippets/plugins/woocommerce/change-variable-product-price.php
+++ snippets/plugins/woocommerce/change-variable-product-price.php
@@ -5,26 +5,8 @@
  * @version 1.0.0
  */
 
-add_filter( 'woocommerce_get_price_html', 'lpb_wc_variable_price_html', 10, 2 );
-function lpb_wc_variable_price_html ( $price, $product ) {
-	if ( ! $product->is_type( 'variable' ) || empty( $product->get_price() ) ) return $price;
-
-	// The Prefix
-	$prefix = __( 'A partir de', 'woocommerce' );
-
-	$result = '';
-	$prices = $product->get_variation_prices( true );
-
-	if ( ! empty( $prices['price'] ) ) {
-		$min_price = current( $prices['price'] );
-		$max_price = end( $prices['price'] );
-
-		if ( empty( $min_price ) || $min_price !== $max_price ) {
-			$result .= '<span class="from">' . $prefix . ' </span>';
-		}
-
-		$result .= woocommerce_price( $min_price );
-	}
-
-	return $result;
+// "A partir de {price}"
+add_filter( 'woocommerce_format_price_range', 'lpb_custom_range_price', 10, 3 );
+function lpb_custom_range_price ( $price, $from, $to ) {
+	return sprintf( 'A partir de %s', is_numeric( $from) ? wc_price( $from ) : $from );
 }
