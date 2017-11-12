6490ed522a1dda00d824761f2c67f16da084897c
Luiz Paulo "Bills <luizpbills@gmail.com>
2017-11-12 12:24:14 -0200

    Update disable-widgets.php

--- snippets/disable-widgets.php
+++ snippets/disable-widgets.php
@@ -3,9 +3,9 @@
  * How to disable a widget.
  * Note: there is a plugin too https://wordpress.org/plugins/wp-widget-disable/ 
  * 
- * @version 1.0.0
+ * @version 1.0.1
  */
-add_action( 'widgets_init', 'lpb_unregister_wp_widgets' );
+add_action( 'widgets_init', 'lpb_unregister_wp_widgets', 20 );
 function lpb_unregister_wp_widgets () {
 	unregister_widget( 'WP_Widget_Tag_Cloud' );
 }
