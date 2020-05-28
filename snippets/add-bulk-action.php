eb8c7ce63d95028b5c09a32f42d3f7647f901df3
Luiz "Bills <luizbills@pm.me>
2020-05-28 20:11:38 -0300

    Create add-bulk-action.php

---
+++ snippets/add-bulk-action.php
@@ -0,0 +1,17 @@
+<?php
+
+add_filter( 'bulk_actions-edit-shop-order', 'register_actions' );
+function register_actions ( $actions ) {
+  $actions[ 'YOUR_ACTION_ID' ] = __( 'Gerar Etiquetas do Correios' );
+  return $actions;
+}
+
+add_filter( 'handle_bulk_actions-edit-post', 'handle_actions', 10, 3 );
+function handle_actions ( $redirect_to, $action, $post_ids ) {
+  if ( $action !== 'YOUR_ACTION_ID' ) {
+    // do something
+  }
+  return $redirect_to;
+}
+
+// read more: https://make.wordpress.org/core/2016/10/04/custom-bulk-actions/
