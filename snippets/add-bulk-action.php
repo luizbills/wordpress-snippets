1bff7eceec9d509a15f38076fa27e6ec35af91f5
Luiz "Bills <luizbills@pm.me>
2022-06-08 11:17:41 -0300

    Update add-bulk-action.php

--- snippets/add-bulk-action.php
+++ snippets/add-bulk-action.php
@@ -8,7 +8,7 @@ function register_actions ( $actions ) {
 
 add_filter( 'handle_bulk_actions-edit-post', 'handle_actions', 10, 3 );
 function handle_actions ( $redirect_to, $action, $post_ids ) {
-  if ( $action !== 'YOUR_ACTION_ID' ) {
+  if ( $action === 'YOUR_ACTION_ID' ) {
     // do something
   }
   return $redirect_to;
